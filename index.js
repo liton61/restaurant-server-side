85bd1efdf6951e544348a58c571a9ae88b11e15b
liton61 <litonofficial61@gmail.com>
2023-11-13 17:06:28 +0600

    get method added for menu

--- index.js
+++ index.js
@@ -26,6 +26,15 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
         await client.connect();
+
+        const menuCollection = client.db("restaurantDB").collection('menu');
+
+        // get method for menu
+        app.get('/menu', async (req, res) => {
+            const result = await menuCollection.find().toArray();
+            res.send(result)
+        })
+
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
